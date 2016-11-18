3fd1488a3222e0d8a23e2736656480c100063b7a
Jayakrishnan S <jayakrishnans@qburst.com>
2016-11-18 17:25:41 +0530

    cell editing

--- basic/script/basic.js
+++ basic/script/basic.js
@@ -1,15 +1,79 @@
+var editor; // use a global for the submit and return data rendering in the examples
+ 
 $(document).ready(function() {
-    $('#example').DataTable( {
-        "ajax": '../assets/data/data.json',
-        "fnRowCallback": function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
-        	if(aData[2] == 'Tokyo'){
-            	$(nRow).css('background-color', '#DFF2BF').css('color', '#4F8A10').css('font-weight', 'bold'); 
-            }else if( aData[2] == 'New York' ){
-            	nRow.cells[2].style.color = 'red';            	
+    editor = new $.fn.dataTable.Editor( {
+        ajax: "../server/staff.php",
+        table: "#example",
+        fields: [ {
+                label: "First name:",
+                name: "first_name"
+            }, {
+                label: "Last name:",
+                name: "last_name"
+            }, {
+                label: "Position:",
+                name: "position"
+            }, {
+                label: "Office:",
+                name: "office"
+            }, {
+                label: "Extension:",
+                name: "extn"
+            }, {
+                label: "Start date:",
+                name: "start_date",
+                type: "datetime"
+            }, {
+                label: "Salary:",
+                name: "salary"
             }
-            if(aData[1] == 'Software Engineer'){
-            	nRow.cells[1].style.background = '#4F8A10';
+        ]
+    } );
+ 
+    // Activate an inline edit on click of a table cell
+    $('#example').on( 'click', 'tbody td:not(:first-child)', function (e) {
+        editor.inline( this, {
+            buttons: { label: '&gt;', fn: function () { this.submit(); } }
+        } );
+    } );
+ 
+    $('#example').DataTable( {
+        dom: "Bfrtip",
+        ajax: "../assets/data/data.json",
+        "fnRowCallback": function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
+            if(aData.office == 'Tokyo'){
+                $(nRow).css('background-color', '#DFF2BF').css('color', '#4F8A10').css('font-weight', 'bold'); 
+            }else if( aData.office == 'New York' ){
+                nRow.cells[4].style.color = 'red';              
+            }
+            if(aData.position == 'Software Engineer'){
+                nRow.cells[3].style.background = '#4F8A10';
+                nRow.cells[3].style.color = '#ffffff';
             }
         },
+        columns: [
+            {
+                data: null,
+                defaultContent: '',
+                className: 'select-checkbox',
+                orderable: false
+            },
+            { data: "first_name" },
+            { data: "last_name" },
+            { data: "position" },
+            { data: "office" },
+            { data: "start_date" },
+            { data: "salary", render: $.fn.dataTable.render.number( ',', '.', 0, '$' ) }
+        ],
+        order: [ 1, 'asc' ],
+        select: {
+            style:    'os',
+            selector: 'td:first-child'
+        },
+        buttons: [
+            { extend: "create", editor: editor },
+            { extend: "edit",   editor: editor },
+            { extend: "remove", editor: editor }
+        ]
     } );
 } );
